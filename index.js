f795d2ede1370ac2d604f8069941cebfab06129c
Moman Amjad <momanamjad07@gmail.com>
2026-06-25 00:04:15 +0500

    fix: restrict body parser request limit to 1mb

--- index.js
+++ index.js
@@ -133,8 +133,8 @@ app.use('/api/auth/register', authLimiter);
 app.use(cookieParser());
 
 // Body Parser Middleware
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ limit: '1mb', extended: true }));
 
 // Disable query buffering so that we don't hang for 10s if the connection fails or isn't ready
 mongoose.set('bufferCommands', false);
